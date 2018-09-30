560debcdce446cbc645e2f121807b9549c3a6ae2
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-09-30 18:17:56 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index] <= 8 || board[index] >= 0 || position_taken? == false
+  if board[index] <= 8 && board[index] >= 0 && position_taken? == false
      true
   end
 end
